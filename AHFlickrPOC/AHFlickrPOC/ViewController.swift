39666f150fd32e382a79472aaf1a66d31c60710e
Piyush <piyush.dwivedi@infogain.com>
2020-10-28 14:55:56 +0530

    Flickr Project initiated

---
+++ AHFlickrPOC/AHFlickrPOC/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AHFlickrPOC
+//
+//  Created by Piyush on 28/10/20.
+//  Copyright © 2020 athenahealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
+
---
+++ AHFlickrPOC/AHFlickrPOC/Views/AHSearchCell.swift
@@ -0,0 +1,17 @@
+//
+//  AHSearchCell.swift
+//  AHFlickrPOC
+//
+//  Created by Piyush on 28/10/20.
+//  Copyright © 2020 athenahealth. All rights reserved.
+//
+
+import UIKit
+
+class AHSearchCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
